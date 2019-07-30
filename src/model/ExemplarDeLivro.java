7e25cbc2c5aebd5c4598ddad8ca2db40ea8843c7
Miguel <miguel.reis@tripletech.com.br>
2019-07-30 08:51:01 -0300

    added: corpo da classe ExemplarDeLivro

--- src/model/ExemplarDeLivro.java
+++ src/model/ExemplarDeLivro.java
@@ -2,8 +2,65 @@ package model;
 
 public class ExemplarDeLivro {
 
-	public ExemplarDeLivro() {
-		// TODO Auto-generated constructor stub
+	protected int id;
+	
+	protected boolean disponivel;
+	
+	protected boolean reservado;
+	
+	protected int edicao;
+	
+	protected String editora = "";
+	
+	protected int anoDeLancamento;
+	
+	public int getId() {
+		return id;
 	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isDisponivel() {
+		return disponivel;
+	}
+
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+
+	public boolean isReservado() {
+		return reservado;
+	}
+
+	public void setReservado(boolean reservado) {
+		this.reservado = reservado;
+	}
+
+	public int getEdicao() {
+		return edicao;
+	}
+
+	public void setEdicao(int edicao) {
+		this.edicao = edicao;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public int getAnoDeLancamento() {
+		return anoDeLancamento;
+	}
+
+	public void setAnoDeLancamento(int anoDeLancamento) {
+		this.anoDeLancamento = anoDeLancamento;
+	}
+
+	
 }
