4a8b46cd30c6c28ef2ba878c1ce5c5ea568ca841
dreis0 <migueldreis01@gmail.com>
2019-08-03 13:25:32 -0300

    updated: mensagem da exception

--- src/Principal.java
+++ src/Principal.java
@@ -1,18 +1,17 @@
 import model.Aluno;
-import model.ExemplarDeLivro;
-import model.Livro;
 import model.Usuario;
-import txtRepository.ExemplarDeLivroRepository;
-import txtRepository.LivroRepository;
 import txtRepository.UsuarioRepository;
 
 public class Principal {
 
 	public static void main(String[] args) {
-
 		try (UsuarioRepository repository = UsuarioRepository.instance()) {
 			Usuario a = new Aluno();
-
+			
+//			switch
+//				normal ->  classe telas de usuario normal -> na classe instancia o action
+//				
+			
 			a.setNome("Miguel");
 			a.setSenha("1234");
 			a.setDocumento("11201721111");
--- src/exceptions/UsuarioJaExisteException.java
+++ src/exceptions/UsuarioJaExisteException.java
@@ -1,8 +1,8 @@
 package exceptions;
 
 public class UsuarioJaExisteException extends Exception {
-    
-    public UsuarioJaExisteException(String email) {
-        super("O usuario " + email + " j√° existe");
-    }
+
+	public UsuarioJaExisteException(String email) {
+		super("O " + email + " j· est· cadastrado");
+	}
 }
